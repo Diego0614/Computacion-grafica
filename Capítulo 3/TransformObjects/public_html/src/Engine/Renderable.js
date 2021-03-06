bc4bbc8dc378b6a3143e25b30f703c7f571aeb68
Diego Muñoz <diego.munoz.arce@correounivalle.edu.co>
2021-03-06 14:46:23 -0500

    Capitulo 3

---
+++ Capítulo 3/TransformObjects/public_html/src/Engine/Renderable.js
@@ -0,0 +1,30 @@
+
+function Renderable(shader) {
+    this.mShader = shader; // the shader for shading this object
+    this.mColor = [1, 1, 1, 1]; // Color for fragment shader   
+
+
+    this.mXform = new Transform(); // transform operator for the object
+}
+Renderable.prototype.getXform = function () {
+    return this.mXform;
+};
+
+
+Renderable.prototype.draw = function (modelTransform) {
+    var gl = gEngine.Core.getGL(); // always activate the shader first!
+    this.mShader.activateShader(this.mColor);
+
+
+    // always activate the shader first!
+    this.mShader.loadObjectTransform(this.mXform.getXform());
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+};
+
+
+Renderable.prototype.setColor = function (color) {
+    this.mColor = color;
+};
+Renderable.prototype.getColor = function () {
+    return this.mColor;
+};
